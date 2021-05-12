b1fbe9c2c7156f000c8540a9de2cc49440ea3bda
chenzhixia <chenzhixia@gemdale.com>
2021-05-12 14:19:55 +0800

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
+import store from './store';
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App),
+}).$mount('#app');
