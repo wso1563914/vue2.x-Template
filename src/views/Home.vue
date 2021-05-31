d170fa0e308c13d0d2d268884de60e445cb086e0
linjunteng <linjunteng1@gemdale.com>
2021-05-31 18:13:35 +0800

    新增select modal组件示例

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="home">
         <i-button @click="show">Click me!</i-button>
+        <i-button @click="handleHere">Click here!</i-button>
+        <select-modal v-model="selectedItems"/>
     </div>
 </template>
 
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
+    import { SelectModal } from 'gem-select-modal';
 
     @Component({
-        components: {},
+        components: {
+            SelectModal
+        },
     })
     export default class Home extends Vue {
         value = [20, 50];
-
+        // hello = {};
+        selectedItems = [];
         show(): void {
             this.$router.push({ name: 'personnelTrack' });
+        }
+        handleHere(): void {
+
         }
     }
 </script>
